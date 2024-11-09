e2b2932b38597167a714d40a59adbf28da1db211
Ашурков Евгений <biatl0n@yandex.ru>
2024-11-09 22:18:08 +0500

    Переписал на Английский тексты

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="csrf-param" content="_token">
 
-        <title>{{ config('app.name', 'Менеджер задач') }}</title>
+        <title>{{ __('Task Manager') }}</title>
 
         <!-- Fonts -->
         <link rel="dns-prefetch" href="//fonts.gstatic.com">
