d4cae8fe492cd58105c159797597aee532c870bd
Ашурков Евгений <biatl0n@yandex.ru>
2025-01-26 21:10:11 +0500

    Изменил регистр текста кнопок. Добавил в en немного переводов.

---
+++ lang/en/task-status.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'create-status' => 'new',
+    'change-status' => 'change',
+    'created-successfully' => 'Статус успешно создан',
+    'changed-successfully' => 'Статус успешно изменён',
+    'deleted-successfully' => 'Статус успешно удалён',
+    'deleted-fail-is-used' => 'Не удалось удалить статус',
+    'already-exists' => 'Статус с таким именем уже существует',
+];
