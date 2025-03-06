2697ccb55790a0c37c6a4dfa7dbfd90cfaa7e52f
Ашурков Евгений <biatl0n@yandex.ru>
2025-03-07 02:46:36 +0500

    Fix code Smells

--- lang/ru/validation.php
+++ lang/ru/validation.php
@@ -1,19 +1,7 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Validation Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines contain the default error messages used by
-    | the validator class. Some of these rules have multiple versions such
-    | as the size rules. Feel free to tweak each of these messages here.
-    |
-    */
-
-    'accepted' => 'The :attribute field must be accepted.',
+    'accepted' => 'Поле :attribute должно быть принято.',
     'accepted_if' => 'The :attribute field must be accepted when :other is :value.',
     'active_url' => 'The :attribute field must be a valid URL.',
     'after' => 'The :attribute field must be a date after :date.',
